2be644591e648bd7011c45e5a602dbea835827fe
KJ Price <kjprice12@gmail.com>
2023-06-01 10:03:06 -0500

    Store session in mongodb

--- middleware/session.js
+++ middleware/session.js
@@ -1,6 +1,10 @@
+const MongoStore = require('connect-mongo');
 const session = require("express-session");
 
 const expressSession = session({
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://127.0.0.1:27017/stephenChat'
+  }),
   secret: "stephenandkjsupersecretproject",
   resave: false,
   saveUninitialized: true,
