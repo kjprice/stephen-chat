314123c61eecc33bb0c182fd515bc6222d3d25a6
KJ Price <kjprice12@gmail.com>
2023-06-01 10:54:25 -0500

    Cleanup

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-// const session = require("express-session");
 const expressSession = require("./middleware/session");
 const initializeSocketIo = require("./sockets/socketConnection");
 const port = process.env.PORT || 4321;
 
-// app.set('trust proxy', 1) // trust first proxy
-
-app.use(express.static("public"));
 app.use(expressSession);
 io.engine.use(expressSession);
 
+app.use(express.static("public"));
 initializeSocketIo(io);
 
 http.listen(port, () => {
